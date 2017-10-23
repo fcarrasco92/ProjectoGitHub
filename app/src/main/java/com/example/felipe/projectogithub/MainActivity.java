f96222ba86bf698240d2995c3d1f56cdac5fb87f
felipe <felipe123>
2017-10-22 21:38:29 -0300

    Subiendo cambios

--- app/src/main/java/com/example/felipe/projectogithub/MainActivity.java
+++ app/src/main/java/com/example/felipe/projectogithub/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Comprobando que funciona nuestro version de control", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
